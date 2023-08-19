fcaea5f3884dba9208cbb7044b49bde929913ee4
dlopez <dalro1028@hotmail.com>
2023-08-19 16:29:14 -0500

    Agrego comentarios

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	//Crea un router con gin
 	router := gin.Default()
 
-	//Captura la solicitud GET "/hello-world"
+	//Captura la solicitud GET "/hola-mundo"
 	router.GET("/hola-mundo", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"mensaje": "¡Hola Mundo!",
